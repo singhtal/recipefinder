2c3da49c71ec81106ef070af24ab02a8a31197b1
Tal <talvindersingh1992@gmail.com>
2019-09-22 23:07:33 +0530

    First commit

---
+++ build/precache-manifest.02e98b0dff5274d17090b6023ec6bb07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccab5002ccf61f8b7eabb7d5887486a",
+    "url": "/recipe-finder/index.html"
+  },
+  {
+    "revision": "d254c6e8986c8db955b3",
+    "url": "/recipe-finder/static/css/main.fc8b5cd1.chunk.css"
+  },
+  {
+    "revision": "be4d2d3d90a325dc5b24",
+    "url": "/recipe-finder/static/js/2.4a8b84db.chunk.js"
+  },
+  {
+    "revision": "d254c6e8986c8db955b3",
+    "url": "/recipe-finder/static/js/main.03374353.chunk.js"
+  },
+  {
+    "revision": "6703937583e1cff8620a",
+    "url": "/recipe-finder/static/js/runtime-main.e419971c.js"
+  }
+]);
